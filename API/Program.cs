48c9c2ceee389c2524803e33dd1638278012d0dd
Jason <hicksj10@gmatc.matc.edu>
2022-10-03 12:01:41 -0500

    Completed Project

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,16 @@
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>();
 
 var app = builder.Build();
 
@@ -16,7 +21,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+
+app.UseCors(policy => policy
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+
 
 app.UseAuthorization();
 
--- Domain/WeatherForecast.cs
+++ Domain/WeatherForecast.cs
@@ -1,12 +1,14 @@
-namespace API;
+namespace Domain;
 
 public class WeatherForecast
 {
+    public int Id { get; set; }
+
     public DateTime Date { get; set; }
 
     public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public Boolean WillAttend => Random.Shared.Next(2) == 1;
 
     public string? Summary { get; set; }
 }
